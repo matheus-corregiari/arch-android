69079f8075a7cb35b953fff188192e501647ea84
Matheus Barbieri Corregiari <metheus.92@gmail.com>
2026-02-13 11:24:08 -0300

    Add RecyclerView adapter utilities and sticky header support
    - Implement `BaseRecyclerAdapter` and `SimpleAdapter` using `AsyncListDiffer` to support `ViewBinder` based custom views.
    - Add `StickyHeadersLinearLayoutManager` to provide sticky header functionality for `RecyclerView`.
    - Define `StickyHeaders` and `StickyHeaderModel` interfaces to identify and manage sticky items within adapters.
    - Implement `SimpleStickyAdapter` to simplify the creation of lists with sticky headers and standard items.
    - Include `BaseViewHolder`, `DefaultItemDiffer`, and `ViewBinder` interfaces to standardize adapter implementation.
    - Support `ConcatAdapter` and custom sticky header evaluations within the layout manager.

---
+++ android/src/androidMain/kotlin/br/com/arch/toolkit/android/recyclerAdapter/BaseViewHolder.kt
@@ -0,0 +1,9 @@
+package br.com.arch.toolkit.android.recyclerAdapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Basic ViewHolder used in BaseRecyclerAdapter
+ */
+class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view)
---
+++ android/src/androidMain/kotlin/br/com/arch/toolkit/android/recyclerAdapter/stickyheader/StickyHeaderModel.kt
@@ -0,0 +1,5 @@
+package br.com.arch.toolkit.android.recyclerAdapter.stickyheader
+
+interface StickyHeaderModel {
+    var isSticky: Boolean
+}
